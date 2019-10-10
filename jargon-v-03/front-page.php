f6c8f8aa8d3abd457beb843d2a070d2b452d69f9
kevinlqc <43006978+kevinlqc@users.noreply.github.com>
2019-10-10 12:31:21 -0600

    3

---
+++ jargon-v-03/front-page.php
@@ -0,0 +1,11 @@
+<?php 
+    get_header();
+?>
+
+<main>
+    front-page template
+</main>
+
+<?php 
+    get_footer();
+?>
---
+++ jargon-v-03/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Jargoon</title>
+    <?php wp_head();?>
+</head>
+
+<body>
+
+    <nav class="site-nav">
+        <ul class="icon-view">
+            <li class="brand"><img src="https://kevinlqc.com/dmit2032/wp-content/uploads/2019/10/logo-1.svg" alt="jargon logo">icon</li>
+            <li class="menu-icons"><img src="https://kevinlqc.com/dmit2032/wp-content/uploads/2019/10/bars.svg" alt="menu icon">menu</li>
+        </ul>
+    </nav>
+    <header class="main-page-header">
+        <img src="https://res.cloudinary.com/kevinlqc/image/upload/c_scale,f_auto,q_auto,w_1920/v1570574116/jargon/uncompressed_ghauta" alt="background header">
+    </header>
